7668709ae6f64c2cc4b4334b4f0a013f7173df3c
Jesus Amaya <jamaya2015@hotmail.com>
2015-03-08 16:30:54 -0600

    finished httpClient*

--- learnyounode/httpClient.js
+++ learnyounode/httpClient.js
@@ -3,7 +3,10 @@ var http = require('http');
 var url = process.argv[2];
 
   http.get(url,function(res){
-    console.log('this is the Response', res);
-  }).on('data',function(dato){
-    console.log('Data at onData level', dato);
-  });
+    //console.log('this is the Response', res);
+      res.setEncoding('utf8');
+      res.on('data',function(dato){
+          console.log(dato);
+      });
+      res.on('error',function(err){console.error('ERROR',err)});
+  })
