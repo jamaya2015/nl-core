4f86c297f10834a80fe5749726d1ec87d73321c6
Jesus Amaya <jamaya2015@hotmail.com>
2015-03-07 18:05:04 -0600

    finished 06

--- learnyounode/06main.js
+++ learnyounode/06main.js
@@ -2,8 +2,16 @@ var fs = require('fs');
 var path = require('path');
 var dirname=process.argv[2];
 var fext=process.argv[3];
-var filArray=[];
 var fun=require('./06module');
 
 
+fun(dirname,fext,function(err,data){
+  if(err){
+    console.log('There was a error',err);
+  }else{
+    data.forEach(function(elem){
+      console.log(elem);
+    });
+  }
 
+});
--- learnyounode/06module.js
+++ learnyounode/06module.js
@@ -1,21 +1,16 @@
 module.exports = function( dirname, fext,callback){
 var filArray=[];
-var fun=require('./06module');
 
-
-fs.readdir(dirname ,
-       function (err, list) {
+fs.readdir(dirname , function (err, list) {
          for(var i=0; i<list.length; i++){
            if(path.extname(list[i]) === '.' + fext ){
              filArray.push(list[i]);
            }
          }
-         
-         filArray.forEach(function(elem){
-           console.log(elem);
-         });         
-       }
-     )
-
-
+         if(err){
+           callback(err);
+         }else{
+           callback(fillArray);
+         }
+       })
 }
